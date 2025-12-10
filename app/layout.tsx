4316b558666ddafc17f4c04cc7d579172d24cb5b
Abdulkerim <kerimewa@gmail.com>
2025-12-10 15:18:56 +0300

    add josefinSans font from Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Logo from "./components/Logo";
 import {Navigation} from "./components/Navigation";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -23,6 +14,11 @@ export const metadata: Metadata = {
     "Discover the beauty of untouched nature and find your own wild oasis.",
 };
 
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary-950 text-primary-100 `}
+        className={`${josefinSans.className} antialiased bg-primary-950 text-primary-100 `}
       >
         <header>
           <Logo />
