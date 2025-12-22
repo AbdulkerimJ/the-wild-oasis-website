ac80796b654404bb2afebd78a34ddac4a24bf063
Abdulkerim <kerimewa@gmail.com>
2025-12-22 19:31:51 +0300

    fix image rendering issue in cabin page

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,5 +1,6 @@
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 type PageProps = {
   params: Promise<{ cabinId: string }>;
@@ -16,14 +17,13 @@ const cabin = await getCabin(Number(cabinId));
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <img src={image} alt={`Cabin ${name}`} />
+          <Image fill  src={image} alt={`Cabin ${name}`} className="object-cover" />
         </div>
 
         <div>
           <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
             Cabin {name}
           </h3>
-
           <p className="text-lg text-primary-300 mb-10">{description}</p>
 
           <ul className="flex flex-col gap-4 mb-7">
