ae97bde2dd37c223baca3e26d103003a25b2325c
Abdulkerim <kerimewa@gmail.com>
2026-01-06 22:17:50 +0300

    implement delete reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { signIn, signOut } from "./auth";
 import { auth } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -45,4 +46,22 @@ export async function updateGuest(formData) {
   }
   revalidatePath("/account/profile");
   redirect("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be logged in to delete a reservation.");
+  }
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You can only delete your own reservations.");
+  }
+  
+  const { error } = await supabase.from("bookings").delete().eq("id", bookingId);
+
+  if (error)
+    throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
 }
--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -1,14 +1,16 @@
+import { getBookings } from "@/app/_lib/data-service";
 import ReservationCard from "../../_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import Link from "next/link";
 export const metadata = {
   title: "Your Reservations",
   description: "Manage your cabin reservations.",
 };
 
-
-export default function Page() {
-  // CHANGE
-  const bookings = [];
-
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session?.user?.guestId);
+  console.log(bookings[0]);
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
@@ -18,9 +20,9 @@ export default function Page() {
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
-          <a className="underline text-accent-500" href="/cabins">
+          <Link className="underline text-accent-500" href="/cabins">
             luxury cabins &rarr;
-          </a>
+          </Link>
         </p>
       ) : (
         <ul className="space-y-6">
